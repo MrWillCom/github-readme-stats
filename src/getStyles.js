9f4a7559d2ba379ba03003e2ffbf0f78c2b819e5
MrWillCom <mr.will.com@outlook.com>
2021-03-07 21:28:35 +0800

    Update labels

--- src/getStyles.js
+++ src/getStyles.js
@@ -65,6 +65,7 @@ const getStyles = ({
     }
     
     .bold { font-weight: 700 }
+    .label { color: #666 }
     .icon {
       fill: ${iconColor};
       display: ${!!show_icons ? "block" : "none"};
